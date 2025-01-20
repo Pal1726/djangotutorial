0daaf3045ecb70bff0afff0044c662d7a448ae49
Palak <palakrathiiit@gmail.com>
2025-01-20 18:22:49 +0530

    add

--- polls/models.py
+++ polls/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
     def __str__(self):
-    	return self.question_text
+        return self.question_text
 
     @admin.display(
         boolean=True,           # Shows as a True/False column
@@ -25,4 +25,4 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __str__(self):
-    	return self.choice_text
+        return self.choice_text
